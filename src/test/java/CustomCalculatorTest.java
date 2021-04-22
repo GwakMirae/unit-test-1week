39f8d41928b3bc5d2e87d11f749b026a5b0799a4
GwakMirae <alfo9490@naver.com>
2021-04-22 22:48:31 +0900

    finish

--- src/test/java/CustomCalculatorTest.java
+++ src/test/java/CustomCalculatorTest.java
@@ -11,28 +11,31 @@ public class CustomCalculatorTest {
 
     //더하기 테스트 작성
     @Test
-    public void add() {
+    public void addTest() {
         customCalculator = new CustomCalculator();
         int result = customCalculator.add(10,15);
         /* 이곳에 테스트 코드를 작성하세요. */
+        assertTrue(result == 25);
         System.out.println("result :: " + result);
     }
 
     //빼기 테스트 작성
     @Test
-    public void subtract() {
+    public void subtractTest() {
         customCalculator = new CustomCalculator();
         int result = customCalculator.subtract(23,10);
         /* 이곳에 테스트 코드를 작성하세요. */
+        assertTrue(result == 13);
         System.out.println("result :: " + result);
     }
 
     //곱하기 테스트 작성
     @Test
-    public void multiply() {
+    public void multiplyTest() {
         customCalculator = new CustomCalculator();
         int result = customCalculator.multiply(5,9);
         /* 이곳에 테스트 코드를 작성하세요. */
+        assertTrue(result == 45);
         System.out.println("result :: " + result);
     }
 
@@ -42,6 +45,7 @@ public class CustomCalculatorTest {
         customCalculator = new CustomCalculator();
         int result = customCalculator.divide(25,5);
         /* 이곳에 테스트 코드를 작성하세요. */
+        assertTrue(result == 5);
         System.out.println("result :: " + result);
     }
 }
